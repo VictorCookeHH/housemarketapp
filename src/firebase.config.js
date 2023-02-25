0645d6d749f6ef160db293e4e80c841dfc556ba8
Victor <victor@humminghomes.com>
2023-02-25 18:11:19 -0300

    Remove keys from config file

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -4,12 +4,7 @@ import { getFirestore } from "firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBjFz_PvHWMJiJxywQAN1yx3_gHtTrlAOc",
-  authDomain: "house-marketplace-app-5a52f.firebaseapp.com",
-  projectId: "house-marketplace-app-5a52f",
-  storageBucket: "house-marketplace-app-5a52f.appspot.com",
-  messagingSenderId: "839807513962",
-  appId: "1:839807513962:web:1fe34f7132de65e2a5a113",
+  //Config data here
 }
 
 // Initialize Firebase
